9f0ec58c8795860b483be9d495a347fb8606e91c
Marvin <malee31@s.sfusd.edu>
2019-10-03 22:22:16 -0700

    Okay parsing txt to question and answer, gotta format. Possible issue: Miss an answer and it'll use the answer to the next question instead...

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -5,13 +5,22 @@ import Question from 'components/Question'
 
 const allFaqs=faq.split("\n");
 var faqObj=[];
+
+//turns allFaqs from array of every line in ../faq.txt into faqObj filled with {question, answer} objects
 for(var i=1; i<allFaqs.length; i++)
 {
 	if(allFaqs[i-1].trim().toLowerCase()=="question")
 	{
-		faqObj.push({question: allFaqs[i], answer: allFaqs[i+2]});
+		for(var ans=i; ans<allFaqs.length; ans++)
+		{
+			if(allFaqs[ans].trim().toLowerCase()=="answer")
+			{
+				faqObj.push({question: allFaqs[i], answer: allFaqs[ans+1]});
+			}
+		}
 	}
 }
+
 const Wrapper = styled.div`
   display: flex;
   margin: 0.5rem 0;
