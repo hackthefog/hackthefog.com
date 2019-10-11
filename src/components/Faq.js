7ba6e99dcc65eae77bd9f6ddb7e9be54b5632a7b
Marvin <43216761+malee31@users.noreply.github.com>
2019-10-10 21:31:05 -0700

    Fixed up text parsing and added borderline no style to it. Commented code to be understandable

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -4,32 +4,47 @@ import faq from 'raw-loader!../faq.txt'
 import Question from 'components/Question'
 
 const allFaqs=faq.split("\n");
-var faqObj=new Array();
+var faqObj=[];
 
-//turns allFaqs from array of every line in ../faq.txt into faqObj filled with {question, answer} objects
-for(var i=1; i<allFaqs.length; i++)
+//turns allFaqs from array of every line in ../faq.txt into faqObj filled with {question, [answer]} objects
+for(var currentLine=1; currentLine<allFaqs.length; currentLine++)
 {
-	faqObj[faqObj.length]={question: allFaqs[i], answer: []};
-	for(var ans=i; ans<allFaqs.length; ans++)
-	{
-		if(allFaqs[ans]==="")
-		{
-			break;
-		}
-		faqObj[faqObj.length-1].answer[faqObj[faqObj.length-1].answer.length]=allFaqs[ans];
-	}
+  //If line before current was a blank
+  if(allFaqs[currentLine-1].trim()==="")
+  {
+    //make new object in array to array.map() and increment current line so that the question doesn't become part of the answer
+  	faqObj[faqObj.length]={question: allFaqs[i], answer: []};
+    i++;
+  	for(var ans=currentLine; ans<allFaqs.length; ans++)
+  	{
+      //loops and adds each following line as another array item in faqObj.answer[] until it finds next blank line
+  		if(allFaqs[ans].trim()==="")
+  		{
+  			break;
+  		}
+  		faqObj[faqObj.length-1].answer[faqObj[faqObj.length-1].answer.length]=allFaqs[ans];
+  	}
+  }
 }
 
 const Wrapper = styled.div`
-  display: flex;
-  width: calc(100vw-200px);
-  margin: 0.5rem 0;
+	display: flex;
+	width: calc(100vw-200px);
+	margin: 0.5rem 0;
+	background-color: white;
+	min-height: 20vh;
+	padding: 0px auto;
+	background-color: #AAA;
+	border-radius: 25px;
+	flex-wrap: wrap;
+	align-content: flex-start;
+	justify-content: space-around;
 `
 
 export default () => (
-  <Wrapper>
-	{faqObj.map(faqSet => (
-		<Question question={faqSet.question} answer={faqSet.answer} />
-	))}
-  </Wrapper>
+	<Wrapper>
+		{faqObj.map(faqSet => (
+			<Question question={faqSet.question} answer={faqSet.answer} />
+		))}
+	</Wrapper>
 )
