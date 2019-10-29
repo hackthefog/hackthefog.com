07c64833fe7a871f63f75d09f9ee9bea7008535a
Marvin <43216761+malee31@users.noreply.github.com>
2019-10-28 20:07:12 -0700

    Removed random comments

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -31,27 +31,6 @@ function processFAQs()
     }
   }
 }
-  
-  // for(var currentLine=1; currentLine<allFaqs.length; currentLine++)
-  // {
-  //   //If line before current was a blank
-  //   if(allFaqs[currentLine-1].trim()==="")
-  //   {
-  //     //make new object in array to array.map() and increment current line so that the question doesn't become part of the answer
-  //   	faqObj[faqObj.length]={question: allFaqs[currentLine], answer: []};
-  //     currentLine++;
-  //   	for(var ans=currentLine; ans<allFaqs.length; ans++)
-  //   	{
-  //       //loops and adds each following line as another array item in faqObj.answer[] until it finds next blank line
-  //   		if(allFaqs[ans].trim()==="")
-  //   		{
-  //   			break;
-  //   		}
-  //   		faqObj[faqObj.length-1].answer[faqObj[faqObj.length-1].answer.length]=allFaqs[ans];
-  //   	}
-  //   }
-  // }
-}
 
 const Wrapper = styled.div`
 	display: flex;
