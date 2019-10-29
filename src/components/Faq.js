8791b1779305c106b5dea9d24106d960fd18f558
Marvin <43216761+malee31@users.noreply.github.com>
2019-10-28 20:05:26 -0700

    Made the loop that creates the array of objects less messy

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,30 +1,56 @@
 import React from 'react'
 import styled from 'styled-components'
+//Imports faq.txt into one mega string
 import faq from 'raw-loader!../faq.txt'
 import Question from 'components/Question'
 
-const allFaqs=faq.split("\n");
+//used in making FAQ objects
 var faqObj=[];
 
-//turns allFaqs from array of every line in ../faq.txt into faqObj filled with {question, [answer]} objects
-for(var currentLine=1; currentLine<allFaqs.length; currentLine++)
+//Splits the faq lines into objects with {question: "String", answer: ["Array", "of", "Strings"]}
+function processFAQs()
 {
-  //If line before current was a blank
-  if(allFaqs[currentLine-1].trim()==="")
+  //Separates the long one string into lines
+  const allFaqs=faq.split("\n");
+  var answerArr=[];
+  for(var currentLine=1; currentLine<allFaqs.length; currentLine++)
   {
-    //make new object in array to array.map() and increment current line so that the question doesn't become part of the answer
-  	faqObj[faqObj.length]={question: allFaqs[currentLine], answer: []};
-    currentLine++;
-  	for(var ans=currentLine; ans<allFaqs.length; ans++)
-  	{
-      //loops and adds each following line as another array item in faqObj.answer[] until it finds next blank line
-  		if(allFaqs[ans].trim()==="")
-  		{
-  			break;
-  		}
-  		faqObj[faqObj.length-1].answer[faqObj[faqObj.length-1].answer.length]=allFaqs[ans];
-  	}
+    if(allFaqs[currentLine-1].trim()==="")
+    {
+      //Prevents loop from accessing faqObj[-1]
+      if(faqObj.length>0)
+      {
+        faqObj[faqObj.length-1].answer=answerArr;
+      }
+      faqObj.push({question: allFaqs[currentLine], answer: []});
+      answerArr=[];
+    }
+    else
+    {
+      answerArr.push(allFaqs[currentLine]);
+    }
   }
+}
+  
+  // for(var currentLine=1; currentLine<allFaqs.length; currentLine++)
+  // {
+  //   //If line before current was a blank
+  //   if(allFaqs[currentLine-1].trim()==="")
+  //   {
+  //     //make new object in array to array.map() and increment current line so that the question doesn't become part of the answer
+  //   	faqObj[faqObj.length]={question: allFaqs[currentLine], answer: []};
+  //     currentLine++;
+  //   	for(var ans=currentLine; ans<allFaqs.length; ans++)
+  //   	{
+  //       //loops and adds each following line as another array item in faqObj.answer[] until it finds next blank line
+  //   		if(allFaqs[ans].trim()==="")
+  //   		{
+  //   			break;
+  //   		}
+  //   		faqObj[faqObj.length-1].answer[faqObj[faqObj.length-1].answer.length]=allFaqs[ans];
+  //   	}
+  //   }
+  // }
 }
 
 const Wrapper = styled.div`
@@ -41,6 +67,7 @@ const Wrapper = styled.div`
   justify-content: center;
 `
 
+processFAQs();
 export default () => (
 	<Wrapper>
 		{faqObj.map(faqSet => (
