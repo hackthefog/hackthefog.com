5f4225b4137b2af63ca3d3ac137a0f015e84656d
Alex Ruiz <me@ruizalex.com>
2019-11-12 21:41:26 -0800

    Change front

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,25 +1,59 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Heading, Lead } from 'components/Text'
+import { Big } from 'components/Text'
 import '../../../static/fonts/mercury/index.css'
+import { theme } from 'theme'
+
+import mblogo from '../../img/missionbit.png'
+import logo from '../../img/logo.svg'
 
 const Wrapper = styled.div`
-  padding-bottom: 2rem;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+`
+const Presenters = styled.div`
+  img {
+    height: 3rem;
+  }
+  span {
+    white-space: pre;
+  }
+  font-weight: 700;
+  font-size: 8vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: ${theme.colors.second[1]};
+  -webkit-text-fill-color: transparent;
+  background: -webkit-linear-gradient(${theme.colors.second[0]}, ${theme.colors.second[1]});
+  background: -o-linear-gradient(transparent, transparent);
+  -webkit-background-clip: text;
+
+  ${theme.mediaQueries.lg} {
+    color: #000;
+    font-size: 4vw;
+    img { height: 4vw; }
+  }
 `
 
 export default () => (
   <Wrapper>
-    <Heading>
-      San Francisco’s first high school hackathon is back.
-    </Heading>
-    <Lead style={{marginTop: '3rem'}}>
-      In March 2018, we made history with Hack the Fog 1.0, San Francisco’s
-      first high school hackathon — it was a massive success.
-      <span style={{display:'block', marginTop: '1rem'}}>
-        Hack the Fog 2.0 is happening on February 15-16, 2020. Join us for
-        another wonderful night of hacking and creativity! Sign up below and
-        we’ll email you when registration opens.
-      </span>
-    </Lead>
+    <Presenters>
+      <img src={mblogo} />
+      <span> + </span>
+      <img src={logo} />
+      <span> present</span>
+    </Presenters>
+    <Big style={{color: theme.colors.first, width: "100%", textAlign: "center"}}>
+      Hack the Fog 2.0
+    </Big>
   </Wrapper>
 )
