e1bbb9566f861b2fc8a8fea684d552e76d2a9966
Alex Ruiz <me@ruizalex.com>
2019-11-22 15:08:46 -0800

    Better 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 import Layout from '../components/Layout'
-import { Big, Subheading } from '../components/Text'
+import { Heading, Subheading } from '../components/Text'
 import { Link } from '../components/Link'
 import { theme } from 'theme'
 
 const Wrapper = styled.div`
-  background-color: ${theme.colors.primary};
   text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
 `
 
 export default () => (
-  <Wrapper>
-    <Layout>
-      <Big>404!</Big>
+  <Layout>
+    <Wrapper>
+      <Heading>404!</Heading>
       <Subheading>
-        Sorry, that page isn’t here.{' '}
-        <Link href="/" style={{ color: theme.colors.white }}>
+        Sorry, that page isn’t here.<br />
+        <Link href="/" style={{ color: theme.colors.gray[9] }}>
           Go home »
         </Link>
       </Subheading>
-    </Layout>
-  </Wrapper>
+    </Wrapper>
+  </Layout>
 )
