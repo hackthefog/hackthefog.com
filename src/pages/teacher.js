636e2dd90f22ad0055b01b998f392aedd0ac65d8
Alexander Ruiz <me@ruizalex.com>
2020-02-04 14:34:44 -0800

    Add teacher typeform

---
+++ src/pages/teacher.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import {GlobalStyle} from 'theme'
+
+const Div = styled.div`
+  margin: 0;
+  height: 100vh;
+  overflow: hidden;
+  iframe{
+    position: absolute;
+    left:0;
+    right:0;
+    bottom:0;
+    top:0;
+    border:0;
+  }
+`;
+
+export default (props) => {
+  return (
+    <Div>
+      <GlobalStyle/>
+      <iframe title="teacher-typeform" id="typeform-full" width="100%" height="100%" frameborder="0" src="https://hackthefog.typeform.com/to/MlmV3F"></iframe>
+    </Div>
+  )
+};
